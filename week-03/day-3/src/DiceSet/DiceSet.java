fa89cfef476d1fd607f277f23433bad667768f3d
Adam Kocso <adam@kocso.com>
2019-07-10 16:30:52 +0200

    Dice Set

---
+++ week-03/day-3/src/DiceSet/DiceSet.java
@@ -0,0 +1,37 @@
+package DiceSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiceSet {
+    ArrayList<Integer> dice = new ArrayList<>();
+
+    DiceSet(){}
+
+    public List<Integer> roll() {
+        for (int i = 0; i < 6; i++) {
+            dice.add((int) (Math.random() * 6) + 1);
+        }
+        return dice;
+    }
+
+    public List<Integer> getCurrent() {
+        return dice;
+    }
+
+    public int getCurrent(int i) {
+        return dice.get(i);
+    }
+
+    public void reroll() {
+        for (int i = 0; i < dice.size(); i++) {
+            dice.set(i, (int) (Math.random() * 6) + 1);
+        }
+    }
+
+    public void reroll(int k) {
+        dice.set(k, (int) (Math.random() * 6) + 1);
+    }
+
+
+}
---
+++ week-03/day-3/src/DiceSet/Main.java
@@ -0,0 +1,29 @@
+package DiceSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // You have a `DiceSet` class which has a list for 6 dice
+        // You can roll all of them with roll()
+        // Check the current rolled numbers with getCurrent()
+        // You can reroll with reroll()
+        // Your task is to roll the dice until all of the dice are 6
+
+        DiceSet diceSet = new DiceSet();
+        System.out.println(diceSet.getCurrent());
+        System.out.println(diceSet.roll());
+        for (int i = 0; i < 6; i++) {
+                while(diceSet.dice.get(i) != 6) {
+                    diceSet.reroll(i);
+                }
+
+        }
+
+//        System.out.println(diceSet.getCurrent());
+//        System.out.println(diceSet.getCurrent(5));
+//        diceSet.reroll();
+//        System.out.println(diceSet.getCurrent());
+//        diceSet.reroll(4);
+        System.out.println(diceSet.getCurrent());
+    }
+}
