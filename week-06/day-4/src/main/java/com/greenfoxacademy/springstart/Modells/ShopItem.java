b5454892902890d748bcece97803f99d01ee1b1e
Adam Kocso <adam@kocso.com>
2019-08-09 17:38:27 +0200

    Basic webshop optional

--- week-06/day-4/src/main/java/com/greenfoxacademy/springstart/Modells/ShopItem.java
+++ week-06/day-4/src/main/java/com/greenfoxacademy/springstart/Modells/ShopItem.java
@@ -4,13 +4,15 @@ package com.greenfoxacademy.springstart.Modells;
 
 public class ShopItem {
   String name;
+  String type;
   String description;
   double price;
   int quantityOfStock;
 
 
-  public ShopItem(String name, String description, double price, int quantityOfStock) {
+  public ShopItem(String name, String type, String description, double price, int quantityOfStock) {
     this.name = name;
+    this.type = type;
     this.description = description;
     this.price = price;
     this.quantityOfStock = quantityOfStock;
@@ -31,5 +33,13 @@ public class ShopItem {
 
   public int getQuantityOfStock() {
     return quantityOfStock;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
   }
 }
--- week-06/day-4/src/main/java/com/greenfoxacademy/springstart/controllers/ShopItemController.java
+++ week-06/day-4/src/main/java/com/greenfoxacademy/springstart/controllers/ShopItemController.java
@@ -2,22 +2,28 @@ package com.greenfoxacademy.springstart.controllers;
 
 import com.greenfoxacademy.springstart.Modells.ShopItem;
 import com.greenfoxacademy.springstart.Modells.Storage;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.lang.Integer.parseInt;
+
 @Controller
 public class ShopItemController {
   Storage list = new Storage();
-  ShopItem shopItem1 = new ShopItem("Running shoes", "Nike running shoes for every day sport", 1000.0, 5);
-  ShopItem shopItem2 = new ShopItem("Printer", "Some HP printer that will print pages", 3000.0, 2);
-  ShopItem shopItem3 = new ShopItem("Coca cola", "0.5l standard coke", 25.0, 0);
-  ShopItem shopItem4 = new ShopItem("Wokin", "Chicken with fried rice and WOKIN sauce", 119.0, 100);
-  ShopItem shopItem5 = new ShopItem("T-shirt", "Blue with a corgi on bike", 300.0, 1);
+  ShopItem shopItem1 = new ShopItem("Running shoes", "Clothes and Shoes", "Nike running shoes for every day sport", 1000.0, 5);
+  ShopItem shopItem2 = new ShopItem("Printer", "Electronics", "Some HP printer that will print pages", 3000.0, 2);
+  ShopItem shopItem3 = new ShopItem("Coca cola", "Beverages and Snacks",
+          "0.5l standard coke", 25.0, 0);
+  ShopItem shopItem4 = new ShopItem("Wokin", "Beverages and Snacks",
+          "Chicken with fried rice and WOKIN sauce", 119.0, 100);
+  ShopItem shopItem5 = new ShopItem("T-shirt", "Clothes and Shoes","Blue with a corgi on bike", 300.0, 1);
 
 
   public void table(){
@@ -29,8 +35,17 @@ public class ShopItemController {
 
   }
 
-  @GetMapping(value = "")
+  @GetMapping(value = "/webshop")
   public String tableItems(Model model){
+    list.getStorage().clear();
+    table();
+    List<ShopItem> allItems = list.getStorage();
+    model.addAttribute("list", allItems);
+    return "index";
+  }
+
+  @GetMapping(value = "")
+  public String main(Model model){
     list.getStorage().clear();
     table();
     List<ShopItem> allItems = list.getStorage();
@@ -81,7 +96,6 @@ public class ShopItemController {
             .mapToDouble(number -> number.getQuantityOfStock())
             .average()
             .getAsDouble();
-
     model.addAttribute("average", "Average stock: " + average);
     return "average";
   }
@@ -113,6 +127,66 @@ public class ShopItemController {
             .collect(Collectors.toList());
     model.addAttribute("list", searchItem);
     return "index";
+  }
+
+
+  @GetMapping("/more-filters")
+  public String tablefilterItems(Model model){
+    list.getStorage().clear();
+    table();
+    List<ShopItem> allItems = list.getStorage();
+    model.addAttribute("list", allItems);
+    return "more";
+  }
+
+
+  @RequestMapping("/filter-by-type")
+  public String clothesAndShoes(Model model, @RequestParam("filter") String filter){
+    list.getStorage().clear();
+    table();
+    List<ShopItem> clothesFilter = list.getStorage().stream()
+            .filter(x -> x.getType().equals(filter.replace('+',' ')))
+            .collect(Collectors.toList());
+    model.addAttribute("list", clothesFilter);
+    return "more";
+  }
+
+
+//  @RequestMapping("/price-in-eur")
+//  public String priceInEur(Model model){
+//    list.getStorage().clear();
+//    table();
+//    List<ShopItem> eur = list.getStorage().stream()
+//            .peek(x -> x.setPrice(x.getPrice() * 0.0387136))
+//            .collect(Collectors.toList());
+//    model.addAttribute("list", eur);
+//    return "more";
+//
+//  }
+
+
+  @PostMapping("/filter-by-price")
+  public String priceFilter(Model model, @RequestParam("searchprice") String searchprice,
+                            @RequestParam("pricefilter") String pricefilter) {
+    list.getStorage().clear();
+    table();
+    List<ShopItem> searchPrice = new ArrayList<>();
+
+    if(pricefilter.equals("Above")) {
+      searchPrice = list.getStorage().stream()
+              .filter(x -> x.getPrice() > parseInt(searchprice))
+              .collect(Collectors.toList());
+    } else if(pricefilter.equals("Below")) {
+      searchPrice = list.getStorage().stream()
+              .filter(x -> x.getPrice() < parseInt(searchprice))
+              .collect(Collectors.toList());
+    } else if(pricefilter.equals("Exactly")) {
+      searchPrice = list.getStorage().stream()
+              .filter(x -> x.getPrice() == parseInt(searchprice))
+              .collect(Collectors.toList());
+    }
+    model.addAttribute("list", searchPrice);
+    return "more";
   }
 
 }
