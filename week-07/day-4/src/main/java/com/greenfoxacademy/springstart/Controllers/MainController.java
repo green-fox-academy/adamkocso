2062a586134a5865a99e368322686c83eb399ca1
Adam Kocso <adam@kocso.com>
2019-08-15 15:08:36 +0200

    feature(Login Page): done

--- week-07/day-4/src/main/java/com/greenfoxacademy/springstart/Controllers/MainController.java
+++ week-07/day-4/src/main/java/com/greenfoxacademy/springstart/Controllers/MainController.java
@@ -1,13 +1,31 @@
 package com.greenfoxacademy.springstart.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
   @GetMapping("/")
-  public String renderingMainPage(){
+  public String renderingMainPage(@RequestParam(value = "name", required = false) String name, Model model){
+    if(name == null){
+      return "redirect:/login";
+    }
+    model.addAttribute("name", name);
     return "index";
   }
+
+  @GetMapping("/login")
+  public String renderingLoginPage(){
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String setFoxName(@RequestParam("name") String name){
+    return "redirect:/?name=" + name;
+  }
+
 }
