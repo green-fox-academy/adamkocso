aa7a5ceb32398375e384cf4ab504c63c9cf5c9d2
Adam Kocso <adam@kocso.com>
2019-08-22 16:02:05 +0200

    Listing Todos H2

---
+++ week-08/day-4/ListingTodosH2/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
@@ -0,0 +1,29 @@
+package com.greenfoxacademy.springstart;
+
+import com.greenfoxacademy.springstart.models.Todo;
+import com.greenfoxacademy.springstart.repositories.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstartApplication implements CommandLineRunner {
+
+  TodoRepository todoRepository;
+
+  @Autowired
+  public SpringstartApplication(TodoRepository todoRepository) {
+    this.todoRepository = todoRepository;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringstartApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    todoRepository.save(new Todo("Get sleep"));
+    todoRepository.save(new Todo("Homework"));
+  }
+}
---
+++ week-08/day-4/ListingTodosH2/src/main/java/com/greenfoxacademy/springstart/models/Todo.java
@@ -0,0 +1,55 @@
+package com.greenfoxacademy.springstart.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+  @Id
+  @GeneratedValue
+  private long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+
+  public Todo(String title) {
+    this.title = title;
+  }
+
+  public Todo() {
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isUrgent() {
+    return urgent;
+  }
+
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
