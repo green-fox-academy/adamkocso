4a565a703a0c282f44d31485e3c8fe758c79e2c9
Adam Kocso <adam@kocso.com>
2019-08-22 17:45:38 +0200

    Modification

--- week-08/day-4/ListingTodosH2/src/main/java/com/greenfoxacademy/springstart/repositories/TodoRepository.java
+++ week-08/day-4/ListingTodosH2/src/main/java/com/greenfoxacademy/springstart/repositories/TodoRepository.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.springstart.repositories;
 
 import com.greenfoxacademy.springstart.models.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
